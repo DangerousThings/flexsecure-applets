0b8b3bc3d9db542c6963c0be66a667d714c40356
Christoph Honal <christoph.honal@web.de>
2022-06-19 21:38:49 +0200

    filter binaries

--- scripts/compile/SmartPGP.sh
+++ scripts/compile/SmartPGP.sh
@@ -4,4 +4,5 @@ mkdir -p /app/src/bin /app/src/applets/SmartPGP/target
 cd /app/src/applets/SmartPGP
 cp /app/src/scripts/compile/res/SmartPGP.build.xml .
 JC_HOME=/app/sdks/jc304_kit ant -buildfile SmartPGP.build.xml
-cp /app/src/applets/SmartPGP/target/*.cap /app/src/bin/
+cd /app/src/applets/SmartPGP/target
+cp *default.cap *large.cap /app/src/bin/
--- scripts/compile/openjavacard-ndef.sh
+++ scripts/compile/openjavacard-ndef.sh
@@ -3,4 +3,5 @@
 mkdir -p /app/src/bin
 cd /app/src/applets/openjavacard-ndef
 ant -DJAVACARD_HOME=/app/sdks/jc222_kit build
-cp /app/src/applets/openjavacard-ndef/build/javacard/*.cap /app/src/bin/
+cd /app/src/applets/openjavacard-ndef/build/javacard
+cp *full.cap *tiny.cap /app/src/bin/

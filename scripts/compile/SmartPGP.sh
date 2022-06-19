4100f62b82f9b276cd85c367efe9c70f5137618e
Christoph Honal <christoph.honal@web.de>
2022-06-19 14:30:43 +0200

    fix pgp build

--- scripts/compile/SmartPGP.sh
+++ scripts/compile/SmartPGP.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mkdir -p /app/src/bin
+mkdir -p /app/src/bin /app/src/applets/SmartPGP/target
 cd /app/src/applets/SmartPGP
 cp /app/src/scripts/compile/res/SmartPGP.build.xml .
 JC_HOME=/app/sdks/jc304_kit ant -buildfile SmartPGP.build.xml

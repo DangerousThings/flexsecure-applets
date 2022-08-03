016f24e0ab305d28cb28ef6f7fe561611f99c2f4
Christoph Honal <christoph.honal@web.de>
2022-08-03 08:23:42 +0200

    use fido-attestation-loader for CI

--- scripts/compile/u2f-javacard.sh
+++ scripts/compile/u2f-javacard.sh
@@ -4,4 +4,4 @@ mkdir -p /app/src/bin /app/src/applets/u2f-javacard/target
 cd /app/src/applets/u2f-javacard
 cp /app/src/scripts/compile/res/u2f-javacard.build.xml .
 JC_HOME=/app/sdks/jc304_kit ant -buildfile u2f-javacard.build.xml
-cp /app/src/applets/u2f-javacard/target/*.cap /app/src/bin/
+cp /app/src/applets/u2f-javacard/target/U2FApplet.cap /app/src/bin/

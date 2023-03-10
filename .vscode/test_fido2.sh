17a7e847156fa1fa7ed8d72621f0a8f27b55e1ec
Christoph Honal <christoph.honal@web.de>
2023-03-10 19:10:02 +0100

    Add fido2 testing script

---
+++ .vscode/test_fido2.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+echo "Making credential"
+CID=`fido2-cred -M -h -r -i test_fido2_make.txt /dev/hidraw5 es256 | sed -n 5p`
+echo "Credential ID: $CID"
+
+echo ""
+echo "Asserting credential"
+fido2-assert -G -h -p /dev/hidraw5 <<EOF
+$(cat test_fido2_make.txt | sed -n 1,2p)
+$CID
+h0nEaICZp9nP48+tS5gr1Vbge6PvwxBBC3LldQjH39gUTsiN5cuoJlKMcqXY5h1k9+HbOb3cP554yx8qULTRLA==
+EOF
--- flake.nix
+++ flake.nix
@@ -24,6 +24,7 @@
             maven
             doxygen
             graphviz
+            libfido2
           ];
         };
     };

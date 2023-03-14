d6520d90115e49075fb466e84e1972b0eab1a640
Christoph Honal <christoph.honal@web.de>
2023-03-14 21:59:27 +0100

    Fix code formatting & fido2 scripts

--- scripts/compile/apex-fido2.sh
+++ scripts/compile/apex-fido2.sh
@@ -2,5 +2,5 @@
 
 mkdir -p /app/src/bin /app/src/applets/apex-fido2/target
 cd /app/src/applets/apex-fido2
-JC_HOME=/app/sdks/jc305_kit ant
+JC_HOME=/app/sdks/jc305u3_kit ant
 cp /app/src/applets/apex-fido2/target/*.cap /app/src/bin/

5b5543932bdabfa6afae096083b94535dad5a07c
Christoph Honal <christoph.honal@web.de>
2023-03-18 06:12:14 +0100

    Update fido2 scripts

--- .vscode/test_fido2.sh
+++ .vscode/test_fido2.sh
@@ -1,13 +1,19 @@
 #!/usr/bin/env bash
 
-echo "Making credential"
-CID=`fido2-cred -M -h -i test_fido2_make.txt /dev/hidraw5 es256 | sed -n 5p`
+echo "Making credential CTAP1"
+CID=`fido2-cred -M -u -i test_fido2_make.txt pcsc://slot2 es256 | sed -n 5p`
 echo "Credential ID: $CID"
 
 echo ""
-echo "Asserting credential"
-fido2-assert -G -h -p /dev/hidraw5 <<EOF
+echo "Asserting credential CTAP1"
+fido2-assert -G -u -p pcsc://slot2 <<EOF
 $(cat test_fido2_make.txt | sed -n 1,2p)
 $CID
-h0nEaICZp9nP48+tS5gr1Vbge6PvwxBBC3LldQjH39gUTsiN5cuoJlKMcqXY5h1k9+HbOb3cP554yx8qULTRLA==
+EOF
+
+echo ""
+echo "Asserting credential CTAP2"
+fido2-assert -G -p pcsc://slot2 <<EOF
+$(cat test_fido2_make.txt | sed -n 1,2p)
+$CID
 EOF

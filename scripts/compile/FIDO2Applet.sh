b57e2fca5f13b163b480c7a418dcaf0cf81a1836
Christoph Honal <christoph.honal@web.de>
2023-11-23 04:53:29 +0100

    compile fido2 applet with different aid

--- scripts/compile/FIDO2Applet.sh
+++ scripts/compile/FIDO2Applet.sh
@@ -2,5 +2,5 @@
 
 mkdir -p /app/src/bin
 cd /app/src/applets/FIDO2Applet
-JC_HOME=/app/sdks/jc304_kit ./gradlew buildJavaCard classes
+JC_HOME=/app/sdks/jc304_kit ./gradlew -PPackageID='A0:00:00:06:47:2F:00:01' -PApplicationID='A0:00:00:06:47:2F:00:01:01' buildJavaCard classes
 cp /app/src/applets/FIDO2Applet/build/javacard/*.cap /app/src/bin/

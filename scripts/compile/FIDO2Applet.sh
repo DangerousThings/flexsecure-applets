7336c4995dc1b71ad337534baf097de1964c60e7
Christoph Honal <christoph.honal@web.de>
2024-08-29 20:13:29 +0200

    Remove FIDO2 AID suffix

--- scripts/compile/FIDO2Applet.sh
+++ scripts/compile/FIDO2Applet.sh
@@ -5,5 +5,5 @@ source "${0%/*}/res/compile.sh"
 prepare_build FIDO2Applet
 patch_version $BUILD/src/main/java/us/q3q/fido2/FIDO2Applet.java
 cd $BUILD
-JC_HOME=/app/sdks/jc304_kit ./gradlew -PPackageID=A0000006472F0001 -PApplicationID=A0000006472F000101 buildJavaCard classes
+JC_HOME=/app/sdks/jc304_kit ./gradlew -PPackageID=A0000006472F00 -PApplicationID=A0000006472F0001 buildJavaCard classes
 cp $BUILD/build/javacard/*.cap /app/src/bin/

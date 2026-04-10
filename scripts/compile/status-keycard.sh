3235f64c10b177b240d234e8eaf9f4ae5fcca4db
Christoph Honal <christoph.honal@web.de>
2026-04-10 18:03:37 +0200

    Fix compilation of new keycard

--- scripts/compile/status-keycard.sh
+++ scripts/compile/status-keycard.sh
@@ -7,6 +7,11 @@ patch_version $BUILD/src/main/java/im/status/keycard/CashApplet.java
 patch_version $BUILD/src/main/java/im/status/keycard/IdentApplet.java
 patch_version $BUILD/src/main/java/im/status/keycard/KeycardApplet.java
 patch_version $BUILD/src/main/java/im/status/keycard/NDEFApplet.java
+rm -rf $BUILD/buildSrc
+sed -i \
+  -e '/im\.status\.keycard\.build\./d' \
+  -e '/if (testTarget == .card.)/,/^}/d' \
+  $BUILD/build.gradle
 cd $BUILD
-JC_HOME=/app/sdks/jc304_kit ./gradlew convertJavacard
+./gradlew convertJavacard
 cp $BUILD/build/javacard/im/status/keycard/javacard/*.cap /app/src/bin/
